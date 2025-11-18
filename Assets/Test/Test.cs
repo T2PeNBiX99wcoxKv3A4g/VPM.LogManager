b71801718873255bf9c35384686a938c8e5754cf
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-11-18 10:53:27 +0800

    fix: Implement `Test` functionality and include necessary assets

---
+++ Assets/Test/Test.cs
@@ -0,0 +1,21 @@
+﻿using io.github.ykysnk.CheatClientProtector;
+using io.github.ykysnk.LogManager;
+using UnityEngine;
+
+namespace Test
+{
+    public class Test : CheatClientProtectorBehaviour
+    {
+        [SerializeField] private LogManager logManager;
+
+        private void Start()
+        {
+            logManager.Log("black", "test", $"test: {this}", logManager.RandomKeyPublic);
+        }
+
+        public override void Interact()
+        {
+            logManager.Log("black", "test", $"test: {this}", logManager.RandomKeyPublic);
+        }
+    }
+}
